5c323b7a8ffbc14d7426e3e81b5b823308462a96
mgtm98 <m.g.t.m.prog@gmail.com>
2019-10-03 04:22:18 +0200

    updated equation of motion

--- src/motion.py
+++ src/motion.py
@@ -6,29 +6,22 @@ from board import SCL, SDA
 import busio
 from adafruit_pca9685 import PCA9685
 
+# removed x_movement and y_movement functions and implemented 
+# the movement functionality in the callback function
 def controlHandeler(pln_motionMsg):
-    x_movement(pln_motionMsg.x_speed)
-    y_movement(pln_motionMsg.y_speed)
+    frontRightMotor.set_speed(pln_motionMsg.y_speed-pln_motion.x_speed)
+    frontLeftMotor.set_speed(pln_motionMsg.y_speed+pln_motion.x_speed)
+    backRightMotor.set_speed(-pln_motionMsg.y_speed-pln_motion.x_speed)
+    backlefttMotor.set_speed(-pln_motionMsg.y_speed+pln_motion.x_speed)
 
 def depthHandeler(z_motionMsg):
     print("code to control rov altitude with data given from depthControl node")
 
-def x_movement(speed):
-    frontRightMotor.clockWise(speed)
-    frontLeftMotor.clockWise(speed)
-    backRightMotor.anticlockWise(speed)
-    backlefttMotor.anticlockWise(speed)
-
-def y_movement(speed):
-    frontRightMotor.anticlockWise(speed)
-    frontLeftMotor.clockWise(speed)
-    backRightMotor.anticlockWise(speed)
-    backlefttMotor.clockWise(speed)
 
 if __name__ == "__main__":
     rospy.init_node("motion")
     rospy.Subscriber("control", pln_motion, controlHandeler)
-    rospy.Subscriber("depthControl", z_motion, depthHandeler)
+    rospy.Subscriber("altitude", z_motion, depthHandeler)
     # hat = adafruit_pca9685.PCA9685()               #initalizing Adafruit hat with default address 0x40
     # hat.setPWMFreq(50)                             #setting pwm freq to 50 hz
     i2c_bus = busio.I2C(SCL, SDA)
