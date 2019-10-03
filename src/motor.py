9463dc71d906275d4c61d6acd9b6cdfc50c204d3
Hassan El-shazly <hassanelshazly2010@gmail.com>
2019-10-03 02:11:56 +0200

    update class motor

--- src/motor.py
+++ src/motor.py
@@ -5,8 +5,14 @@ def map(x, in_min, in_max, out_min, out_max):
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 class Motor:
-	min_value = 1000
-	max_value = 2000
+	# These values have to be calibrated
+	min_cw = 0.7
+	max_cw = 1
+
+	center = 1.5
+
+	min_ccw = 1.7
+	max_ccw = 2
 
 	def __init__(self,hat,channel,initalValue):
 		self.hat = hat
@@ -17,25 +23,32 @@ class Motor:
 	
 	def arm(self):
 		self.updatePWM(0)
-		time.sleep(1)
-		self.updatePWM(max_value)
-		time.sleep(1)
-		self.updatePWM(min_value)
 		time.sleep(0.5)
+		self.updatePWM(max_cw)
+		time.sleep(0.5)
+		self.updatePWM(min_cw)
+		time.sleep(0.5)
+
+	def calibrate(self):
+		# to do
+		pass
 	
 	def clockWise(self, speed):
 		# from calibration of esc we will get the range of values to put
-		speed = map(speed, 0, 1, min_value, max_value)
+		speed = map(speed, 0, 1, min_cw, max_cw)
 		self.updatePWM(speed)
 	
 	def anticlockWise(self, speed):
 		# from calibration of esc we will get the range of values to put
-		speed = map(speed, 0, 1, min_value, max_value)
+		speed = map(speed, 0, 1, min_ccw, max_ccw)
 		self.updatePWM(speed)
 
-	def updatePWM(self,pwm):
+	def stop(self):
+		self.updatePWM(center)
+
+	def updatePWM(self, pwm):
 		self.fallingValue = pwm
 		# self.hat.setPWM(self.channel,0,pwm)
-		self.hat.channels[self.channel].duty_cycle = pwm 
+		self.hat.channels[self.channel].duty_cycle = (pwm / 20) * 0xffff
 
 	
