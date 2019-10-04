f7fb67263a62d938d0ce4541d3f863b1b06479b5
mgtm <m.g.t.m.prog@gmail.com>
2019-10-04 13:56:09 +0200

    added lights service & renamed cameraControl to vision

--- src/vision.py
+++ src/vision.py
@@ -1,4 +1,5 @@
 from rov20.msg import camera_info
+from rov20.srv import lights
 import rospy
 from camera import Camera
 from board import SCL, SDA
@@ -18,9 +19,16 @@ def control(data):
         camera[data.index].stop_stream()
     return
 
+def lights_handeler(data):
+    if(data.command == "on"):
+        print("turining lights on")
+    else :
+        print("turning lights off")
+    return "OK"
+
 if __name__ == "__main__":
-    rospy.init_node("camera")
-    rospy.Subscriber("cameraControl", camera_info, control)
+    rospy.init_node("vision")
+    rospy.Subscriber("camera", camera_info, control)    #topic name is camera not cameraControl
     i2c_bus = busio.I2C(SCL, SDA)
     hat = PCA9685(i2c_bus)
     hat.frequency = 50
@@ -33,5 +41,6 @@ if __name__ == "__main__":
     camera.append(Camera(hat, 6, 7, 0, host, port1))
     camera.append(Camera(hat, 8, 9, 1, host, port1))
 
+    lightsSrv = rospy.Service("lights",lights,lights_handeler)
     rospy.spin()
         
