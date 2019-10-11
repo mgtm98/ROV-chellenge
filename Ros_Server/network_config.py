710e407ce364569333fe93d2e1ea58e0f38c245a
mgtm <m.g.t.m.prog@gmail.com>
2019-10-11 13:37:46 +0200

    updated version of Ros Server

--- Ros_Server/network_config.py
+++ Ros_Server/network_config.py
@@ -1,7 +1,8 @@
 import json
 import threading
 from std_msgs.msg import String, Empty
-from Company.srv import test
+from rov20.msg import pln_motion, raw_data, z_motion
+from rov20.srv import lights
 import time
 
 
@@ -29,51 +30,68 @@ def get_srv_handeler(srv_name):
     return services_handlers_table[srv_name]
 
 # Stuff to add or modify
-def Salary_Handeler(data):
-    response = {
-        "type" : "topic_response",
-        "topic_name" : "Salary",
-        "msg" : {
-            "type" : "std_msgs/Empty",
-        },
-    }
-    for port in subscribers["Salary"][0]:
-        server_obj.send_Data(port, str(json.dumps(response)))
 
-def Time_Handeler(data):
-    response = {
-        "type" : "topic_response",
-        "topic_name" : "Time",
-        "msg" : {
-            "type" : "std_msgs/String",
-            "data" : {
-                "String" : str(time.time())
-            }
-        },
-    }
-    for port in subscribers["Time"][0]:
-        server_obj.send_Data(port, str(json.dumps(response)))
+# # # # # # # # # # # # # # # #  TOPICS SUBSCRIBTION CONFIGRATIONS # # # # # # # # # # # # # # # # #
+# def Salary_Handeler(data):
+#     response = {
+#         "type" : "topic_response",
+#         "topic_name" : "Salary",
+#         "msg" : {
+#             "type" : "std_msgs/Empty",
+#         },
+#     }
+#     for port in subscribers["Salary"][0]:
+#         server_obj.send_Data(port, str(json.dumps(response)))
+#
+# def Time_Handeler(data):
+#     response = {
+#         "type" : "topic_response",
+#         "topic_name" : "Time",
+#         "msg" : {
+#             "type" : "std_msgs/String",
+#             "data" : {
+#                 "String" : str(time.time())
+#             }
+#         },
+#     }
+#     for port in subscribers["Time"][0]:
+#         server_obj.send_Data(port, str(json.dumps(response)))
+subscribe_handlers_table = {
+    # "Salary" : Salary_Handeler,
+    # "Time" : Time_Handeler,
+}
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-def Testsrv_Handeler(func,srvData):
-    output = func(srvData["data"]["input"]).output
+# # # # # # # # # # # # # # # #  SERVICES RESPONSE CONFIGRATIONS  # # # # # # # # # # # # # # # #
+# def Testsrv_Handeler(func,srvData):
+#     output = func(srvData["data"]["input"]).output
+#     return {
+#         "output" : output
+#         }
+#
+def lightssrv_Handeler(func,srvData):
+    output = func(srvData["data"]["command"]).response
     return {
         "output" : output
-        }
-
-subscribe_handlers_table = {
-    "Salary" : Salary_Handeler,
-    "Time" : Time_Handeler,
-}
+    }
 
 services_handlers_table = {
-    "test" : Testsrv_Handeler,
+    # "test" : Testsrv_Handeler,
+    "lights" : lightssrv_Handeler
 }
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
+# # # # # # # # # # # # # # # # GENERAL CONFIGRATIONS # # # # # # # # # # # # # # # # # # # # # #
 msgTypes = {
     "std_msgs/Empty" : Empty,
     "std_msgs/String" : String,
+    "rov20/pln_motion" : pln_motion,
+    "rov20/raw_data" : raw_data,
+    "rov20/z_motion" : z_motion
 }
 
 srvTypes = {
-    "test" : test
+    # "test" : test,
+    "lights" : lights
 }
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
